813497f34704062664d29ecb35956d0315834264
Hammad Nawaz <36544@students.riphah.edu.pk>
2025-05-07 00:15:17 +0500

    login api integration

--- app/(onboarding)/login.tsx
+++ app/(onboarding)/login.tsx
@@ -1,59 +1,51 @@
-import React from "react";
 import { useLoginUserMutation } from "@/api/authentication";
 import Box from "@/components/Box";
 import Button from "@/components/Button";
 import { useForm } from "@/components/Form";
 import TextInput from "@/components/TextInput";
-// import { updateAuth } from "@/redux/slices/auth";
-// import { updateStore, updateToastMessage } from "@/redux/slices/common";
-// import { useTypedDispatch } from "@/redux/store";
-import { isAndroid, validationRules } from "@/utils";
-// import AsyncStorage from "@react-native-async-storage/async-storage";
+import { updateStore } from "@/redux/slices/common";
+import { useTypedDispatch } from "@/redux/store";
+import { isAndroid } from "@/utils";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { router } from "expo-router";
+import React from "react";
 import { SafeAreaView, View } from "react-native";
 import { Text, useTheme } from "react-native-paper";
+import * as Yup from "yup";
+
+const DATA = {
+  email: "",
+  password: "",
+};
+
+const SCHEMA = {
+  email: Yup.string()
+    .email("Email is not valid.")
+    .required("Email is required."),
+  password: Yup.string().required("Password is required."),
+};
 
 const Login = () => {
   const theme = useTheme();
-  // const dispatch = useTypedDispatch();
+  const dispatch = useTypedDispatch();
   const [loginUser, { isLoading }] = useLoginUserMutation();
 
   const { errors, handleChange, handleFoucs, handleSubmit } = useForm({
-    initialValues: {
-      email: "",
-      password: "",
-    },
-    validationRules: validationRules({
-      email: "",
-      password: "",
-    }),
+    initialValues: DATA,
+    validationSchema: Yup.object().shape(SCHEMA),
     onSubmit: (values: any) => {
-      router.replace("/(tabs)");
-      // const result = schema.safeParse(values);
-      // if (!result.success) {
-      //   const errObj: { [key: string]: string } = {};
-      //   result.error.errors.forEach((err) => {
-      //     errObj[err.path[0]] = err.message;
-      //   });
-      //   setErrors(errObj);
-      // } else {
-      //   loginUser(result.data).then((res: any) => {
-      //     if (res.data) {
-      //       dispatch(updateStore({ key: "user", value: res.data }));
-      //       dispatch(updateAuth(res.data.token));
-      //       if (isOnboarding) {
-      //         AsyncStorage.setItem("onboarding-completed", "true");
-      //         router.replace("/(tabs)");
-      //       } else {
-      //         router.dismiss();
-      //       }
-      //     } else {
-      //       dispatch(
-      //         updateToastMessage({ type: "error", message: res.error.data.error })
-      //       );
-      //     }
-      //   });
-      // }
+      loginUser({
+        ...values,
+        email: values.email.toLowerCase(),
+      }).then((res: any) => {
+        if (!res.error) {
+          const { accessToken, refreshToken, ...user } = res.data;
+          dispatch(updateStore({ key: "user", value: user }));
+          AsyncStorage.setItem("access-token", accessToken);
+          AsyncStorage.setItem("refresh-token", refreshToken);
+          router.replace("/(tabs)");
+        }
+      });
     },
   });
 
@@ -90,7 +82,7 @@ const Login = () => {
             autoCapitalize="none"
             keyboardType="email-address"
             onChangeText={handleChange("email")}
-            helperText="Email address is required."
+            helperText={errors.email}
             onFocus={handleFoucs("email")}
             error={Boolean(errors.email)}
           />
@@ -100,7 +92,7 @@ const Login = () => {
               secureTextEntry
               autoCapitalize="none"
               onChangeText={handleChange("password")}
-              helperText="Password is required."
+              helperText={errors.password}
               error={Boolean(errors.password)}
               onFocus={handleFoucs("password")}
             />
@@ -108,7 +100,7 @@ const Login = () => {
               style={{ alignSelf: "flex-end" }}
               textColor={theme.colors.primary}
               onPress={() => {
-                router.push("/register");
+                router.push("/(auth)");
               }}
             >
               Don't have an account? Sign up
