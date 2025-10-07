91c9a960fd18115e159e746d8514db0067d3f724
Hammad Nawaz <36544@students.riphah.edu.pk>
2025-10-07 16:29:13 +0500

    Update login.tsx

--- app/(onboarding)/login.tsx
+++ app/(onboarding)/login.tsx
@@ -1,4 +1,6 @@
 import { useLoginUserMutation } from "@/api/authentication";
+import AuthButton from "@/components/AuthButton";
+import BaseButton from "@/components/BaseButton";
 import Box from "@/components/Box";
 import Button from "@/components/Button";
 import { useForm } from "@/components/Form";
@@ -6,11 +8,13 @@ import TextInput from "@/components/TextInput";
 import { updateStore } from "@/redux/slices/common";
 import { useTypedDispatch } from "@/redux/store";
 import { isAndroid } from "@/utils";
+import { Feather } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { router } from "expo-router";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView, View } from "react-native";
 import { Text, useTheme } from "react-native-paper";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import * as Yup from "yup";
 
 const DATA = {
@@ -27,7 +31,9 @@ const SCHEMA = {
 
 const Login = () => {
   const theme = useTheme();
+  const insets = useSafeAreaInsets();
   const dispatch = useTypedDispatch();
+  const [showPassword, setShowPassword] = useState(false);
   const [loginUser, { isLoading }] = useLoginUserMutation();
 
   const { errors, handleChange, handleFoucs, handleSubmit } = useForm({
@@ -39,11 +45,37 @@ const Login = () => {
         email: values.email.toLowerCase(),
       }).then((res: any) => {
         if (!res.error) {
-          const { accessToken, refreshToken, ...user } = res.data;
-          dispatch(updateStore({ key: "user", value: user }));
-          AsyncStorage.setItem("access-token", accessToken);
-          AsyncStorage.setItem("refresh-token", refreshToken);
-          router.replace("/(tabs)");
+          const {
+            accessToken,
+            refreshToken,
+            userFields = {},
+            userVehicle = {},
+            company = {},
+            ...user
+          } = res.data;
+          if (
+            (user.type === "driver" || user.type === "company") &&
+            user.verificationStatus === "pending"
+          ) {
+            AsyncStorage.setItem("access-token", accessToken);
+            AsyncStorage.setItem("refresh-token", refreshToken);
+            router.replace({
+              pathname: "/verification",
+              params: {
+                ...user,
+                ...userFields,
+                ...userVehicle,
+                ...company,
+                accessToken,
+                refreshToken,
+              },
+            });
+          } else {
+            dispatch(updateStore({ key: "user", value: user }));
+            AsyncStorage.setItem("access-token", accessToken);
+            AsyncStorage.setItem("refresh-token", refreshToken);
+            router.replace("/(tabs)");
+          }
         }
       });
     },
@@ -55,7 +87,7 @@ const Login = () => {
         flex={1}
         px={2.5}
         pt={isAndroid ? 28 / 4 : 4}
-        pb={2.5}
+        pb={isAndroid ? insets.bottom / 6 : 2.5}
         justifyContent="space-between"
       >
         <View style={{ gap: 20 }}>
@@ -88,14 +120,35 @@ const Login = () => {
           />
           <View style={{ gap: 10 }}>
             <TextInput
+              right={
+                <BaseButton
+                  style={{ paddingRight: 12 }}
+                  onPress={() => setShowPassword(!showPassword)}
+                >
+                  <Feather
+                    name={showPassword ? "unlock" : "lock"}
+                    size={18}
+                    color="black"
+                  />
+                </BaseButton>
+              }
               label="Password"
-              secureTextEntry
+              secureTextEntry={!showPassword}
               autoCapitalize="none"
               onChangeText={handleChange("password")}
               helperText={errors.password}
               error={Boolean(errors.password)}
               onFocus={handleFoucs("password")}
             />
+            <Button
+              style={{ alignSelf: "flex-end" }}
+              textColor={theme.colors.primary}
+              onPress={() => {
+                router.push("/requestReset");
+              }}
+            >
+              Forgot Password?
+            </Button>
             <Button
               style={{ alignSelf: "flex-end" }}
               textColor={theme.colors.primary}
@@ -107,24 +160,9 @@ const Login = () => {
             </Button>
           </View>
         </View>
-        <Button
-          textColor="white"
-          mode="contained"
-          loading={isLoading}
-          onPress={handleSubmit}
-          style={{
-            borderRadius: 100,
-            height: 46,
-            justifyContent: "center",
-            minWidth: 130,
-          }}
-          labelStyle={{
-            fontSize: 16,
-            fontWeight: 700,
-          }}
-        >
+        <AuthButton onPress={handleSubmit} loading={isLoading}>
           Login
-        </Button>
+        </AuthButton>
       </Box>
     </SafeAreaView>
   );
